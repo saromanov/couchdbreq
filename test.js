4f8b75a284283d8833153adfe35fc0bc655a9ad2
Sergey Romanov <xxsmotur@gmail.com>
2015-06-04 14:22:30 -0700

    Provide additional tests

--- test.js
+++ test.js
@@ -10,11 +10,28 @@ it('should insert to couchdb', function() {
 
 });
 
+it('should insert with default params', function() {
+    var cdb2 = require('./')()
+    cdb2.insert("item8889", {
+        foo: "bar",
+    }, function(err, statusCode, response) {
+        assert.Equal(console.log(response).ok, true);
+    });
+});
+
 it('should get from couchdb', function() {
     cdb.get("/item8889", function(error, statusCode, response) {
         if (statusCode === 404) {
             return;
         }
         assert.Equal(response.db_name, "item8889");
+    });
+});
+
+it('should delete from couchdb', function() {
+    cdb.del("/item8889", function(err, statusCode, response) {
+        cdb.get("/item8889", function(error, statusCode, response) {
+            assert.Equal(statusCode, 404);
+       });
     });
 });
